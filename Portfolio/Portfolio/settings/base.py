a2aed4bdc5e67d23de7419c72e60dc86a98a1396
moskeyombus <john.wnek@gmail.com>
2013-05-22 00:02:02 -0500

    aaaaand again

--- Portfolio/Portfolio/settings/base.py
+++ Portfolio/Portfolio/settings/base.py
@@ -111,7 +111,7 @@ INSTALLED_APPS = (
     'south',
     'projects',
     'fabric',
-    'uwsgi'
+    'uwsgi',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
