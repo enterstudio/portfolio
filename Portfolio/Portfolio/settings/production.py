6028094b769301b22cacbc26042a7bbe75d796d9
moskeyombus <john.wnek@gmail.com>
2013-05-23 23:20:13 -0500

    updated for live test

--- Portfolio/Portfolio/settings/production.py
+++ Portfolio/Portfolio/settings/production.py
@@ -1,8 +1,13 @@
 from .base import *
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['dev.indratech.net']
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'mv845jh5498gfhKHGBN6hnuyN'
 
 DATABASES = {
     'default': {
@@ -10,11 +15,9 @@ DATABASES = {
         'NAME': 'portfolio_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'portfolio',
-        'PASSWORD': 'e5Df#Df3^7dT5&6r6Y*e',
+        'PASSWORD': 'e5DfDf37djhvVBsT5r6Ye',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = os.environ['SECRETKEY']
